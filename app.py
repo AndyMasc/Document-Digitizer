a927f634cce6f19620f7882f6d3297e122199701
AndyMasc <andy.mascarenhas12@gmail.com>
2025-06-20 16:23:37 +1200

    Succesfully linked css file to html through static folder.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from bottle import route, run, template
+from bottle import route, run, template, view, static_file
 from TextConverter import *
 
 pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'  # for Apple Silicon
 
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./static')
+
 @route('/')
-def ImageToText():
-    image = cv2.imread(getImageFile())
-    processedImage = preprocessImage(image)
-    text = pytesseract.image_to_string(processedImage)
-    return text
+def index():
+    return template('Homepage')
 
 run(host='localhost', port=8080, debug=True, reloader=True)
