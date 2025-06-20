a55bf1ac2d152a0cd31012fce0f373aa5156ebb2
AndyMasc <andy.mascarenhas12@gmail.com>
2025-06-20 14:21:31 +1200

    Added file opening GUI

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import cv2
 import pytesseract
+from tkinter.filedialog import askopenfilename
+
+def getImage():
+    imageFile = askopenfilename()
+    return imageFile
 
 def preprocessImage(image):
     monochromeImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  # Converting image to gray-scale enhances text differentiation
@@ -8,8 +13,10 @@ def preprocessImage(image):
 
 pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'  # for Apple Silicon
 
-image = cv2.imread("/Users/andymascarenhas/Downloads/test.png")
-
+image = getImage()
+image = cv2.imread(image)
 processedImage = preprocessImage(image)
+
 text = pytesseract.image_to_string(processedImage)
+
 print(text)
