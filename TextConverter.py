b5442e7f404d3fd4335a050b6e8c22d53aa3cadf
AndyMasc <andy.mascarenhas12@gmail.com>
2025-06-20 15:07:58 +1200

    Rename main.py to TextConverter.py

--- TextConverter.py
+++ TextConverter.py
@@ -2,21 +2,19 @@ import cv2
 import pytesseract
 from tkinter.filedialog import askopenfilename
 
-def getImage():
+def getImageFile():
     imageFile = askopenfilename()
     return imageFile
 
 def preprocessImage(image):
-    monochromeImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  # Converting image to gray-scale enhances text differentiation
-    normalizedImage = cv2.normalize(monochromeImage, None, 0, 255, cv2.NORM_MINMAX)
+    GreyScaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Converting image to gray-scale enhances text differentiation
+    normalizedImage = cv2.normalize(GreyScaleImage, None, 0, 255, cv2.NORM_MINMAX)
     return normalizedImage
 
 pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'  # for Apple Silicon
 
-image = getImage()
-image = cv2.imread(image)
+image = cv2.imread(getImageFile())
 processedImage = preprocessImage(image)
 
 text = pytesseract.image_to_string(processedImage)
-
 print(text)
