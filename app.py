6a0ed31ec47cfc09c50756c618a99b1fe7631a25
AndyMasc <andy.mascarenhas12@gmail.com>
2025-06-25 21:53:16 +1200

    Scaled homepage for responsiveness across different platforms

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask_dropzone import Dropzone
+from fpdf import FPDF
 import pytesseract
 import cv2
+import re
 import os
+
 pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
@@ -42,18 +44,33 @@ def convertImageToText():
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string(image)
     try:
-        os.remove(target)  # clean up
+        os.remove(target)
     except FileNotFoundError:
         pass
+    print(repr(text))
     return text
 
 def createPDF(content):
-    filePath = os.path.join(app.static_folder, 'ExtractedText.txt')
-    with open(filePath, 'w') as text:
-        text.write(content)
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.add_font('DejaVu', '', '/Users/andymascarenhas/Library/Fonts/DejaVuSansCondensed.ttf', uni=True)
+    pdf.set_font('DejaVu', '', 12)
+    if not content:
+        pdf.cell(0, 10, "No text could be extracted.")
+
+    content = content.replace('\r\n', '\n').replace('\r', '\n') # Normalize all line endings to just \n
+    content = re.sub(r'\n{2,}', '<<<P>>>', content)  # Mark real paragraphs with temporary marker <<<P>>>
+    content = content.replace('\n', ' ')  # Remove all single newlines
+    content = content.replace('<<<P>>>', '\n\n') # Restore paragraph breaks
+    for paragraph in content.split('\n\n'): # Pass the cleanly formatted paragraphs to be added to the PDF.
+        paragraph = paragraph.strip()
+        if paragraph:
+            pdf.multi_cell(0, 10, paragraph)
+
+    filePath = os.path.join(app.static_folder, 'ExtractedText.pdf')
+    pdf.output(filePath)
 
 dropzone = Dropzone(app)
 if __name__ == '__main__':
-    error = ''
     target = ''
     app.run(host='0.0.0.0', debug=True)
