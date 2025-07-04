f8192affb3998965020242e3979df466bca86532
AndyMasc <andy.mascarenhas12@gmail.com>
2025-07-04 14:18:19 +1200

    Redd open with r due to issue

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def uploadPage():
 
 def convertImageToText():
     global imageFilePath
-    with open(imageFilePath, 'r') as image_file:
+    with open(imageFilePath, 'rb') as image_file:
         content = image_file.read()
     image = vision.Image(content=content)
     response = client.document_text_detection(image=image)  # Use document_text_detection for handwriting
